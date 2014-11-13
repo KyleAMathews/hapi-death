8c6ab438f0fd955b378f4c62abff17ba486fb5eb
Kyle Mathews <mathews.kyle@gmail.com>
2014-11-12 16:21:59 -0800

    Initial checking

---
+++ index.js
@@ -0,0 +1,19 @@
+exports.register = function(plugin, options, next) {
+
+  // Wait 10 seconds for existing connections to close then exit.
+  stop = function() {
+    plugin.servers[0].stop({
+      timeout: 10 * 1000
+    }, function() {
+        process.exit();
+      });
+  };
+  process.on('SIGTERM', stop);
+  process.on('SIGINT', stop);
+
+  next();
+};
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
